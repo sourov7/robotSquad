444bb5c06c8d998a769f76500c42ac01d2041008
sourov7 <37473187+sourov7@users.noreply.github.com>
2018-10-17 22:49:48 +0600

    Updates

---
+++ precache-manifest.6fbb251beb228f102a24d52d002ce9f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotSquad/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6c8dabc462bc2d9cfac5",
+    "url": "/robotSquad/static/js/runtime~main.6c8dabc4.js"
+  },
+  {
+    "revision": "119da26256f9c0f02872",
+    "url": "/robotSquad/static/js/main.119da262.chunk.js"
+  },
+  {
+    "revision": "92f51d2ec8515651e78b",
+    "url": "/robotSquad/static/js/1.92f51d2e.chunk.js"
+  },
+  {
+    "revision": "119da26256f9c0f02872",
+    "url": "/robotSquad/static/css/main.91ad9e3c.chunk.css"
+  },
+  {
+    "revision": "92f51d2ec8515651e78b",
+    "url": "/robotSquad/static/css/1.6d0cd1af.chunk.css"
+  },
+  {
+    "revision": "97e46d2d1e3e8e2a0b0a913eaf80d4f4",
+    "url": "/robotSquad/index.html"
+  }
+];
